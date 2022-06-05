147bb4d4300622a979174e594ba1441a5a37cec8
jmacalawa <jmacalawa@futurex.ph>
2022-06-05 11:47:06 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-img src="/background.jpeg" align="center" class="d-flex justify-center align-center" width="100vw" height="100vh" gradient="to top right, rgba(0,0,0,.38), rgba(0,0,0,0.38)"  >
+       <login-container />
+    </v-img>
+</template>
+
+<script>
+import LoginContainer from '../components/login/LoginContainer.vue'
+export default {
+  components: { LoginContainer },
+
+}
+</script>
+
+<style>
+
+</style>
